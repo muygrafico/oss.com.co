8f4fc765cf9818ac464a135d7c540fdea81c4fa8
German Hernandez <muygrafico@gmail.com>
2016-09-04 20:10:52 -0500

    page about us - translated

--- en/about-us.php
+++ en/about-us.php
@@ -9,9 +9,9 @@
     <div class='container'>
       <div class='row text-center'>
         <div class="col-md-6 col-md-offset-3">
-          <h2 class='section-title'>Nosotros</h2>
+          <h2 class='section-title'>About Us</h2>
           <p class='values-text'>
-            <strong>Hacemos bien las cosas para nuestra gente, nuestro ambiente y nuestra sociedad,pero sobre todo para nuestros clientes.</strong>
+            <strong>We do things good for our people, our environment and our society,but especially for our clients.</strong>
           </p>
         </div>
       </div>
@@ -19,36 +19,38 @@
       <div class='row'>
 
         <div class='col-sm-6'>
-          <h4 class='subtitle pipes'>Reseña Histórica OSS</h4>
+          <h4 class='subtitle pipes'>Historical Review - OSS</h4>
           <p>
-            Oilfield Services & Supplies es una empresa de servicios creada en Abril del 2009, con el propósito de prestar servicios a nuestros Clientes de la industria del petróleo y gas, en el monitoreo y prueba de pozos con equipos de nueva tecnología multifásica y en el bombeo de fluidos downhole para el reacondicionamiento de pozos.
+            Oilfield Services & Supplies was established in Colombia in April 2009 with the objective of supplying services for different clients in the oil and gas industry, in the well testing and production monitoring services with new multiphase metering technology and downhole pumping services for well intervention.
           </p><br>
           <p>
-            A mediano plazo OSS se trazo como objetivo corporativo el suministro de soluciones integrales a los Clientes en el área de facilidades tempranas de superficie, mediante el suministro y operación de equipos compactos de rápido despliegue, portátiles, seguros y de alta eficiencia, con el objetivo de proporcionar soluciones integrales que brinden valor agregado a nuestros clientes reduciendo los costos operacionales, mejorando los procesos y minimizando los requerimientos de manteniendo e igualando o superando los estándares de calidad y seguridad de la industria.
+            In the midterm OSS has as main objective to become number one integral solutions provider to our clients in the region covering early production facilities and fast testing services with compact equipment of rapid deployment as well as portable, safe and highly efficient systems. The objective is to supply integral solutions that bring added value to our clients reducing operational cost and capital expenditure surpassing quality and safety standards in the oil and gas industry.
+
           </p><br>
           <p>
-            Para lograr este objetivo Oilfield Services & Supplies cuenta con equipos compactos, portátiles y de nueva tecnología para el tratamiento, producción, inyección y disposición de agua de producción. Contamos con equipos de amplio rango de cubrimiento para la medición y prueba de pozos como son los medidores multifásicos, equipos de prueba de pozos de bajo caudal (desde 10 bfpd) completamente automatizados de fácil despliegue acondicionados para operar en crudos pesados y extra-pesados con sistemas de calentamiento. Igualmente se cuenta con el servicio de suministro de energía eléctrica con generadores con motores a gas en renta mensual o pago por Kw-h generado.
+            In order to achieve this objective Oilfield Services & Supplies has compact equipment as new technology for treatment of produced water, water injection, production monitoring, well testing with multiphase technology and low production monitoring equipment (LPT) suitable to operate in heavy oil, medium and light oil and even gas Wells.To achieve this Oilfield Services & Supplies has compact, portable and new technology equipment for treatment, production, injection and produced water disposal. We have a wide range of equipment for measuring coverage and well testing such as multiphase meters, wells testing equipment low flow (from 10 bfpd) easy to deploy and fully automated conditioned to operate heavy and extra-heavy oil with heating systems. In addition, OSS supplies the service of providing electricity with gas engine generators paid monthly or per kW-h generated.
+
           </p>
         </div>
 
         <div class='col-sm-6 text-center'>
-          <h4 class='subtitle pipes'>Nuestros Valores</h4>
-          <h6>Liderazgo</h6>
-          <p class='values-text'>El coraje para forjar un mejor futuro liderando con ejemplo.</p>
-          <h6>Integridad</h6>
-          <p class='values-text'>Ser real y ante todo, actuar con integridad.</p>
-          <h6>Responsabilidad</h6>
-          <p class='values-text'>Si es para comprometerse, es para mí.</p>
-          <h6>Pasión</h6>
-          <p class='values-text'>Entregado en mente y corazón.</p>
-          <h6>Calidad</h6>
-          <p class='values-text'>Hacemos las cosas bien desde el primer momento.</p>
+          <h4 class='subtitle pipes'>Our Values</h4>
+          <h6>Leadership</h6>
+          <p class='values-text'>The courage to shape a better future led by example.</p>
+          <h6>Integrity</h6>
+          <p class='values-text'>Be real and foremost, act with integrity.</p>
+          <h6>Responsibility</h6>
+          <p class='values-text'>If it is to commit, it is for me.</p>
+          <h6>Passion</h6>
+          <p class='values-text'>Committed in mind and heart.</p>
+          <h6>Quality</h6>
+          <p class='values-text'>We do things right the first time.</p>
           <div class='values-green-box'>
             <ul class='values-list'>
-              <li>Trabajamos en equipo</li>
-              <li>Respetamos a la persona</li>
-              <li>Nos comunicamos abierta y honestamente</li>
-              <li>Nos comprometemos con la comunidad</li>
+              <li>We work together</li>
+              <li>We respect the individual</li>
+              <li>We communicate openly and honestly</li>
+              <li>We engage with the community</li>
             </ul>
           </div>
         </div>
@@ -57,16 +59,16 @@
 
       <div class='row margin-tb-2'>
         <div class='col-sm-6'>
-          <h6>Misión</h6>
+          <h6>Mission</h6>
           <p>
-            La misión de OilField Services & Supplies es ofrecer soluciones integrales a la industria petrolera orientadas a cubrir las necesidades de nuestros clientes con personal idóneo e innovador con un portafolio de servicios, productos de calidad y nueva tecnología; protegiendo así el medio ambiente, proporcionando una rentabilidad creciente y sostenible a nuestros empleados siendo un completo respaldo para su realización y crecimiento profesional.
+            The mission of Oilfield Services & Supplies is to become the preferred service provider to the oil and gas industry aiming to meet the needs of our clients with our qualified and innovative portfolio of quality products and services with new technology; thus protecting the environment, providing a sustainable return and increasing our employees being and support for their professional growth.
           </p>
           <p><br>
-            A través de nuestros servicios y productos proporcionamos valor agregado para el beneficio de nuestros clientes, nuestra gente y la organización.
+            Through our products and services we provide added value for the benefit of our clients, our people and the organization.
           </p><br>
-          <h6>Visión</h6>
+          <h6>Vision</h6>
           <p>
-            Oilfield Services & Supplies y su personal serán reconocidos como los mejores aliados de nuestros clientes y líderes en el suministro de productos, nueva tecnología y en la prestación de servicios a la industria petrolera en Colombia. Nuestro compromiso empieza desde el momento de la creación de Oilfield Services & Supplies y deberá ser consolidada en el 2015 con las siguientes premisas.
+            Oilfield Services & Supplies and its team will be recognized as the best allies of our clients leading the supply of products and services with new technology to the oil and gas industry in Colombia. Our commitment started since the time Oilfield Services & Supplies was created and must be consolidated in 2015 with the following premises.
           </p><br>
 
         </div>
@@ -82,28 +84,28 @@
         <div class='col-sm-6 col-sm-push-6'>
 
           <p class='vision-point'>
-            <strong>Gente:</strong>
+            <strong>People:</strong>
             Proporcionar un excelente ambiente laboral donde nuestro personal se motive a dar lo mejor de sí mismos.
           </p>
 
           <p class='vision-point'>
-            <strong>Portafolio de productos y servicios:</strong>
-            Poner al servicio de nuestros clientes un portafolio selecto de servicios y productos de nueva tecnología que permita ofrecer soluciones integrales a las necesidades planteadas con la mejor calidad.
+            <strong>Porfolio of products and services:</strong>
+            Offer to our clients a selected portfolio of services and products with new technology to provide integral solutions to their needs with best level of quality and safety.
           </p>
 
           <p class='vision-point'>
-            <strong>Aliados:</strong>
-            crecer una importante red de clientes y proveedores que en conjunto permitan crear valor agregado y beneficio mutuo de larga duración.
+            <strong>Allies:</strong>
+            Grow a strong network of clients and suppliers that working along will allow us to create added value and long-term mutual benefits.
           </p>
 
           <p class='vision-point'>
-            <strong>Rentabilidad:</strong>
-            Maximizar el retorno de la inversión a los accionistas, manteniendo al mismo tiempo la integridad en todas nuestras obras y responsabilidades.
+            <strong>Profitability:</strong>
+            Maximize the Return of Investment of our shareholders, while maintaining integrity in all our work and responsibilities.
           </p>
 
           <p class='vision-point'>
-            <strong>Apoyo a la comunidad:</strong>
-             Contribuir al desarrollo social de las comunidades donde operamos garantizando la preservación del medio ambiente.
+            <strong>Community support:</strong>
+             Contribute to the social development of the communities where we operate ensuring the preservation of the environment.
           </p>
 
         </div>
